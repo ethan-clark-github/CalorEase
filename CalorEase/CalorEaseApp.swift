e46e7ec3f47f2822d155349474f28ec8d7932c4b
ethan clark <esclark99@gmail.com>
2023-03-15 09:12:17 -0400

    Initial Commit

---
+++ CalorEase/CalorEaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalorEaseApp.swift
+//  CalorEase
+//
+//  Created by ethan clark on 3/15/23.
+//
+
+import SwiftUI
+
+@main
+struct CalorEaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
